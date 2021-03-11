38c911e2d03ac2850d8f942119d9a2c0aaadc280
Nadiia Yanushevska <nd@dy.kiev.ua>
2021-03-11 09:54:57 +0000

    added pointers

--- test21.cpp
+++ test21.cpp
@@ -7,6 +7,12 @@ int main(int argc, char* argv[])
 {
 	printf("Hello World!\n");
 
+	int Count1, Count2, Count3;
+	int *pCount1, *pCount2, *pCount3;
+	pCount1 = &Count1;
+	pCount2 = &Count2;
+	pCount3 = &Count3;
+
 	return 0;
 }
 
